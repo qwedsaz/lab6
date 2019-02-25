d8dc922896f196394561a1e1b751c98ac8709b64
qwedsaz <46765114+qwedsaz@users.noreply.github.com>
2019-02-25 16:30:54 -0600

    Update matrixArray.c

--- matrixArray.c
+++ matrixArray.c
@@ -7,9 +7,10 @@ void main() {
  int sizeRow = 10;
  int sizeCol = 10;
  int arr[sizeRow][sizeCol];
+ int arrInverse[sizeRow][sizeCol];
  
  multiplicationTable[arr, sizeRow, sizeCol];
- reverseColumn[arr, sizeRow, sizeCol];
+ reverseColumn[arr, sizeRow, sizeCol, arrInverse, sizeRow, sizeCol];
  
  printf("We'll do later\n");
  
