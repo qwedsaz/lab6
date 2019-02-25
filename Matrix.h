2d5c8d513bffa67a8d6c67b60e70a611985f201c
qwedsaz <46765114+qwedsaz@users.noreply.github.com>
2019-02-25 17:28:11 -0600

    Update Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -1,22 +1,9 @@
 /**
  * Generate the multiplication table
  */
-void multiplicationTable(int *arr, sizeRow, sizeCol) {
-  for (int i = 0; i < sizeRow; i++) {
-    for (int j = 0; j < sizeCol; j++) {
-      if (i == 0 || j == 0) {
-        arr[i][j] = i + j;
-      }
-      else {
-        arr[i][j] = i * j;
-      }
-    }
-  }
-  
-  return;
-}
+void multiplicationTable(int row, int col, int arr[row][col]);
 
 /**
  * Reverse the matrix column-wise
  */
-void reverseColumn(int 
+void reverseColumn(int row, int col, int arr[row][col]);
