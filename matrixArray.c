08367183d824cb1105749e636e5ef636ecae2120
qwedsaz <46765114+qwedsaz@users.noreply.github.com>
2019-02-25 16:20:03 -0600

    Create matrixArray.c

---
+++ matrixArray.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "matrices.h"
+
+void main() {
+ int sizeRow = 10;
+ int sizeCol = 10;
+ int arr[sizeRow][sizeCol];
+ 
+ multiplicationTable[arr, sizeRow, sizeCol];
+ reverseColumn[arr, sizeRow, sizeCol];
+ 
+ printf("We'll do later\n");
+ 
+ return;
+}
