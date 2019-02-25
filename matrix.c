2c48ba21835f1e2624587bdb9490d8551d2bc4f0
qwedsaz <46765114+qwedsaz@users.noreply.github.com>
2019-02-25 17:51:58 -0600

    Update matrix.c

--- matrix.c
+++ matrix.c
@@ -18,8 +18,8 @@ void reverseColumn(int row, int col, int arr[row][col], int arrInverse[row][col]
       arrInverse[i][j] = arr[i][j];
     }
   }
-  for (j = 0; j < col / 2; j++) {
-    for (i = 0; i < row; i++) {
+  for (int j = 0; j < col / 2; j++) {
+    for (int i = 0; i < row; i++) {
       int tempVal = arrInverse[i][j];
       arrInverse[i][j] = arrInverse[i][col - j - 1];
       arrInverse[i][col - j - 1] = tempVal;
