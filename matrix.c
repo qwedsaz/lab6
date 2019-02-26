c777bc2d84d8db54c6f44871e33e467b0c8f4436
qwedsaz <46765114+qwedsaz@users.noreply.github.com>
2019-02-26 07:47:55 -0600

    Update matrix.c

--- matrix.c
+++ matrix.c
@@ -4,8 +4,10 @@
 #include "matrix.h"
 
 void multiplicationTable(int row, int col, int arr[row][col]) {
-  for (int i = 0; i < row; i++) {
-    for (int j = 0 ; j < col; j++) {
+  int i = 0;
+  int j = 0;
+  for (i = 0; i < row; i++) {
+    for (j = 0 ; j < col; j++) {
       arr[i][j] = i * j;
     }
   }
@@ -13,13 +15,15 @@ void multiplicationTable(int row, int col, int arr[row][col]) {
 }
 
 void reverseColumn(int row, int col, int arr[row][col], int arrInverse[row][col]) {
-  for (int i = 0; i < row; i++) {
-    for (int j = 0; j < col; j++) {
+  int i = 0;
+  int j = 0;
+  for (i = 0; i < row; i++) {
+    for (j = 0; j < col; j++) {
       arrInverse[i][j] = arr[i][j];
     }
   }
-  for (int j = 1; j < col / 2; j++) {
-    for (int i = 1; i < row; i++) {
+  for (j = 1; j < col / 2; j++) {
+    for (i = 1; i < row; i++) {
       int tempVal = arrInverse[i][j];
       arrInverse[i][j] = arrInverse[i][col - j];
       arrInverse[i][col - j] = tempVal;
